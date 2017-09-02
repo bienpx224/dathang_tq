55fe4950c7eeca6b00c39deca32d0f96cc2b12b4
vinvin1022 <vin_i@foxmail.com>
2017-09-02 14:44:00 +0800

    copyright jiao robots web.config

---
+++ copyright.php
@@ -0,0 +1,24 @@
+<?php 
+/*
+	----------------本系统授权网址----------------	
+	
+	【警告:以下信息请勿自行改动,自行改动后,我们将停止所有售后服务中止合作,不退款!!!】
+	【如果新加或变更网址,必须联系我们来修改，对使用因未授权的网址而造成的损失，我方不负任何责任!!!】
+		
+	只要您购买过授权,并且是您公司旗下网站,都可以联系我们授权网址
+*/
+$url='
+localhost
+127.0.0.1
+zy31
+www.jbuy.com.au
+www.jbuy.hk
+www.weiweilang.com
+jbuy.com.au
+jbuy.hk
+weiweilang.com
+119.23.22.189
+www.super-fast.com.au
+super-fast.com.au
+';
+?>
